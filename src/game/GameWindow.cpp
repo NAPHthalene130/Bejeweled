9ac7f30e221e3c8582730d3e54c9563a8749e447
NAPHthalene130 <1301537275@qq.com>
2025-12-13 01:44:51 +0800

    feat:添加了MenuButton和GameWindow进行测试

--- src/game/GameWindow.cpp
+++ src/game/GameWindow.cpp
@@ -5,6 +5,7 @@
 #include "gameWidgets/SettingWidget.h"
 #include "gameWidgets/StoreWidget.h"
 #include "gameWidgets/RankListWidget.h"
+#include "components/MenuButton.h"
 #include <QMainWindow>
 #include <QVBoxLayout>
 #include <string>
@@ -17,6 +18,23 @@ GameWindow::GameWindow(QWidget* parent, std::string userID) : QMainWindow(parent
     storeWidget = new StoreWidget(this, this);
     rankListWidget = new RankListWidget(this, this);
     switchWidget(menuWidget);
+    
+    resize(1600, 1000);
+    setWindowTitle("宝石迷阵");
+
+    //以下是测试，到时候记得删了
+    MenuButton* btn1 = new MenuButton(200, 60, 24, Qt::white, "开始游戏", this);
+    btn1->move(100, 100);
+    btn1->raise();
+
+    MenuButton* btn2 = new MenuButton(180, 50, 20, QColor("#FFD700"), "排行榜", this);
+    btn2->move(100, 180);
+    btn2->raise();
+
+    MenuButton* btn3 = new MenuButton(150, 40, 16, QColor(200, 200, 255), "退出", this);
+    btn3->move(100, 250);
+    btn3->raise();
+     //以上是测试，到时候记得删了
 }
 std::string GameWindow::getUserID() {
     return userID;
